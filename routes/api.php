a44630f0343a10e13b4c5efda0622c4b3df505a0
hosamof <humam.cp@gmail.com>
2023-01-14 21:34:06 +0300

    install passaport - login & register requests

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::post('/register', 'App\Http\Controllers\Auth\UserAuthController@register');
+Route::post('/login', 'App\Http\Controllers\Auth\UserAuthController@login');
